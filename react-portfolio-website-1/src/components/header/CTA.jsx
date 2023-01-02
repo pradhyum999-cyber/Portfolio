7907424a2a7748cba3d1f7e531d03fc78272f647
p.sourabh.bharadwaj <p.sourabh.bharadwaj@accenture.com>
2023-01-02 17:40:21 +0530

    Header Updated

--- react-portfolio-website-1/src/components/header/CTA.jsx
+++ react-portfolio-website-1/src/components/header/CTA.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import CV from '../../assets/cv.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
-    <a href=''>Download CV</a>
+    <a href={CV} className= 'btn'>Download CV</a>
+    <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
 }
