9e907c1ed4001ae3e6ad14703745e94e06086666
p.sourabh.bharadwaj <p.sourabh.bharadwaj@accenture.com>
2023-01-02 13:46:26 +0530

    Test

---
+++ react-portfolio-website-1/src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+   <header>
+   <div className='.container.header__container'>
+   <h5>Hello I'm</h5>
+   <h1>Pradyumna Bharadwaj</h1>
+   <h5 className='text-light'>Dynamics CRM Developer</h5>
+   </div>
+   </header>
+  )
+}
+
+export default Header
