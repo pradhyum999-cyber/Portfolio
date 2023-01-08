e552fcface0f59b27e587786e187dd126070b88f
p.sourabh.bharadwaj <p.sourabh.bharadwaj@accenture.com>
2023-01-08 15:41:30 +0530

    ind adj of about and exp

--- react-portfolio-website-1/src/components/experience/Experience.jsx
+++ react-portfolio-website-1/src/components/experience/Experience.jsx
@@ -17,7 +17,7 @@ const Experience = () => {
               <div>
                 <h4>HTML</h4>
                 <small className='text-light'>Intermediate</small>
-                </div>
+              </div>
             </article>
             <article className='experience__details'>
               <BsPatchCheckFill className='experience__details-icons' />
